cd8177edec34866bbcab9c0dc3ded46bc184df7b
EmiFree <emifree03@gmail.com>
2025-09-22 19:10:36 -0700

    starting project

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from voicemessages import VoiceMessageSender
+import tkinter as tk
+from tkinter import *
 
 TOKEN = "your_token_here" # Replace with your token
 CHANNEL_ID = 123  # Replace with your channel ID
 
-voicemessage = VoiceMessageSender(TOKEN, CHANNEL_ID)
-voicemessage.SendVoiceMessage("D:\path\to\your\audio\file.mp3")
+master = tk.Tk()
+greeting = tk.Label(text="Hello, Tkinter")
+Label(master, text='Channel Name').grid(row=0)
+Label(master, text='Channel ID').grid(row=1)
+e1 = Entry(master)
+e2 = Entry(master)
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+master.mainloop()
+# voicemessage = VoiceMessageSender(TOKEN, CHANNEL_ID)
+# voicemessage.SendVoiceMessage("D:\path\to\your\audio\file.mp3")
